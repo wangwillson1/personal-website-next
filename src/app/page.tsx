8a65e8ade512bd82948e0a9280515b95eee28126
Willson Wang <willsonwang@willsons-mbp.home>
2023-06-05 23:31:09 -0400

    fixed linter issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,20 +9,20 @@ export default function Home() {
   const [isVisible, setIsVisible] = useState(true);
 
   useEffect(() => { 
+    const listenToScroll = () => {
+      let hiddenHeight = 30;
+      const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  
+      if (winScroll > hiddenHeight) {  
+           isVisible && setIsVisible(false);
+      } else {
+           setIsVisible(true);
+      }  
+    };
+
     window.addEventListener("scroll", listenToScroll);
     return () => {window.removeEventListener("scroll", listenToScroll)}
   }, []);
-  
-  const listenToScroll = () => {
-    let hiddenHeight = 30;
-    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-
-    if (winScroll > hiddenHeight) {  
-         isVisible && setIsVisible(false);
-    } else {
-         setIsVisible(true);
-    }  
-  };
 
   return (
     <main className="mx-20">
@@ -30,8 +30,8 @@ export default function Home() {
         <div className="flex flex-row-reverse w-full max-w-6xl items-center justify-between banner pb-24">
           <Image className="portrait" src="/portrait.jpg"  alt="me" width="325" height="325"/>
           <div>
-            <h1 className='text-6xl md:text-7xl lg:text-8xl'>Hi, I'm <b>Willson</b>!</h1>
-            <p className='text-2xl md:text-3xl lg:text-4xl'>I'm a software engineer at Datadog.</p>
+            <h1 className='text-6xl md:text-7xl lg:text-8xl'>Hi, I&apos;m <b>Willson</b>!</h1>
+            <p className='text-2xl md:text-3xl lg:text-4xl'>I&apos;m a software engineer at Datadog.</p>
             <div className="navbar">
               <p><a href="/resume.pdf" target="_blank">
                 Resume
@@ -68,7 +68,7 @@ export default function Home() {
           <div id="about">
             <h1 className="text-3xl">A little about me</h1>
             <p className="text-2xl">I studied computer science at the University of Waterloo (Canada!) and graduated in 2023.</p>
-            <p className="text-2xl mt-2">I'm a huge foodie and I love exploring restaurants, but I also enjoy cooking, baking, and making lattes!</p>
+            <p className="text-2xl mt-2">I&apos;m a huge foodie and I love exploring restaurants, but I also enjoy cooking, baking, and making lattes!</p>
             <p className="text-2xl mt-2">Other interests include photography, volleyball, skiing, and snowboarding.</p>
           </div>
           <div className="mt-14">
@@ -80,7 +80,7 @@ export default function Home() {
           <div className="mt-14">
             <h1 className="text-3xl">How can I get in touch</h1>
             <p className="text-2xl">
-              Feel free to reach out through <a href="mailto:wang.willson1@gmail.com">email</a> or <a href="https://www.linkedin.com/in/wangwillson1/" target="_blank">Linkedin</a> if you'd like to chat or meet up!
+              Feel free to reach out through <a href="mailto:wang.willson1@gmail.com">email</a> or <a href="https://www.linkedin.com/in/wangwillson1/" target="_blank">Linkedin</a> if you&apos;d like to chat or meet up!
             </p>
           </div>
         </div>
